68f66f616f3b1701b0a734582a7291f60044887f
Felipe Oliveira <165398196+FelipeGorg@users.noreply.github.com>
2024-09-16 09:48:09 -0300

    Update jogoDaAdivinhacao.js

--- AULA 02-09-24/jogoDaAdivinhacao.js
+++ AULA 02-09-24/jogoDaAdivinhacao.js
@@ -13,22 +13,25 @@
 // }
 
 
-function gerarNumAleatorio(){
+function gerarNumAleatorio() {
     const numAleatorio = Math.floor((Math.random() * 10) + 1);
     return numAleatorio
 }
 
-function jogar(){
-    const respostaPessoa = gerarNumAleatorio()
-    do{
-        const palpite = document.getElementById("resposta").value;
-    }while(numAleatorio != palpite)
-
-        if(numAleatorio === palpite){
-            paragrafo.innerText = `O número aleatório é ${numAleatorio} você errou!`
-        }else{
-        paragrafo.innerText = `O número aleatório é ${numAleatorio} você errou!`
+function jogar() {
+    const numero = gerarNumAleatorio()// Guardar o valor do numero aleatório
+    let palpite = ""// primeiro crio o valor vazio, para depois o valor ser agreagado no loop
+    let numeroDeTentativas = 3
+    do {
+        palpite = parseInt(prompt("Adivinhe o número: "))
+        if (numero != palpite) {
+            numeroDeTentativas--
+            alert(`Você errou, você ainda tem ${numeroDeTentativas} tentativas`)
+        } else if (numero === palpite) {
+            alert(`Parabéns, você acertou!`)
+            return
         }
+    } while (numeroDeTentativas > 0)
 }
 jogar()
 
